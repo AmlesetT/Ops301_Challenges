f98026b09046d06b835a484cea5766373dc6e33b
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-08-30 01:31:47 +0000

    Initial commit

---
+++ Var-Log-Syslog.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script Name:                  Ops 301 - Challenge 01
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      08/29/2023
+# Purpose:                      To append the date and time on a bash script.
+# Reason:                       It is important to be able to adjust the time/date when logs are automated.
+
+# Declaration of variables
+year='date +%Y'
+echo $year
+month='date +%m'
+echo $month
+day='date +%d'
+echo $day
+
+hour='date +%H'
+minute='date +%M'
+second='date +%S'
+
+echo "Current Date: $day-$month-$year"
+echo "Current Time: $hour:$minute:$second"
+
+
+echo "Original file before append: "
+cat testfile.txt
+
+
+# the >> append
+echo "My new string with date: 'date'" >> testfile.txt
+echo "Appended file: "
+cat testfile.txt
+
+# End
