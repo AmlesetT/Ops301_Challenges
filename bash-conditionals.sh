45ff20963e7586e8faa5d2e957690f7bbdd7dd09
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-01 04:49:25 +0000

    Initial commit

---
+++ bash-conditionals.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Ops 301 - Challenge 04
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      08/31/2023
+# Purpose:                      To launch a menu system using hello world, ping self, IP info, and exit on a bash script.
+# Reason:                       It is crucial to be able to quickly access computer, network, and/or system information.
+
+
+# Declaration of variables
+
+
+
+
+
+
+
+
+
+# End
