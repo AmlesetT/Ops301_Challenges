af8e60e79481e9676f898be708401b29bc39aede
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-10 09:52:44 +0000

    Initial commit

--- bash-conditionals.sh
+++ bash-conditionals.sh
@@ -9,12 +9,37 @@
 
 # Declaration of variables
 
+while true; do 
+    echo "Hello World! How can I help you today? Choose one of the following options from below: "
+    echo "1. Hello World!"
+    echo "2. Ping your computer"
+    echo "3. Display your computer's IP information"
+    echo "4. Exit"
+    read choice
 
 
+    if [[ $choice == 1 ]]; then
+        echo "Hello World!"
+        read -p "Please Press Enter to Continue"
 
+    elif [[ $choice == 2 ]]; then
+        ping -c 5 127.0.0.1
+        read -p "Please Press Enter to Continue"
 
+    elif [[ $choice == 3 ]]; then
+        ifconfig
+        read -p "Please Press Enter to Continue"
 
+    elif [[ $choice == 4 ]]; then
+        echo "Exiting."
+        exit 0
+    
+    else
+        echo "Invalid Choice"
+        read -p "Please Press Enter to Continue"
 
+    fi 
 
+done
 
 # End
