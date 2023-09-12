f923f930cb5f029b0dbc56e98b1a1fcb62c1551e
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-12 06:53:57 +0000

    Initial commit

---
+++ psutil.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops 301 - Challenge 11
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      09/11/2023
+# Purpose:                      To create a script in Python that fetches certain information using Psutil.
+# Reason:                       It is important to quickly and efficiently use code to pull necessary information.
+
+
+# Declaration of variables
+
+
+
+Create a Python script that fetches this information using Psutil:
+
+Time spent by normal processes executing in user mode
+Time spent by processes executing in kernel mode
+Time when system was idle
+Time spent by priority processes executing in user mode
+Time spent waiting for I/O to complete.
+Time spent for servicing hardware interrupts
+Time spent for servicing software interrupts
+Time spent by other operating systems running in a virtualized environment
+Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+
+
+
+
+
+
+
+
+
+
+# End
