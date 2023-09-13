14ab00fa6be1b206f5b96d4aed0a1c2d2d2870f3
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-13 06:55:49 +0000

    Initial commit

---
+++ python_requests_library.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Script Name:                  Ops 301 - Challenge 12
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      09/12/2023
+# Purpose:                      To create a script using requests from the Python library.
+# Reason:                       It is crucial to understand the evaluation of web server requests and responses.
+
+
+# Declaration of variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# End
