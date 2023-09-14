c0497245ecc889d855d336881f3559691db5c820
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-14 06:16:44 +0000

    Initial commit

---
+++ powershell_ad_automation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script Name:                  Ops 301 - Challenge 13
+# Author:                       Amleset Tesfamariam
+# Date of latest revision:      09/13/2023
+# Purpose:                      To create a script using Powershell to add a new user to the Active Directory.
+# Reason:                       It is important to know how to create, edit, add, and remove users from the Active Directory as a network administrator.
+
+
+# Declaration of variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# End
