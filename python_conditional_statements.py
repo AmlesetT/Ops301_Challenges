89692cd9f281f6aeec090449c4f3e1b2876d1f66
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-23 23:16:37 +0000

    Initial commit

--- python_conditional_statements.py
+++ python_conditional_statements.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env python3
 
 # Script Name:                  Ops 301 - Challenge 09
 # Author:                       Amleset Tesfamariam
@@ -10,16 +10,25 @@
 # Declaration of variables
 
 
+# Define two variables a and b
+a = 100
+b = 1
 
-
-
-
-
-
-
-
-
-
+# If statements using logical conditionals 
+if a == b:
+    print("a is equal to b")
+elif a != b:
+    print("a is not equal to b")
+elif a < b:
+    print("a is less than b")
+elif a <= b:
+    print("a is less than or equal to b")
+elif a > b:
+    print("a is greater than b")
+elif a >= b:
+    print("a is greater than or equal to b")
+else:
+    print("None of the conditionals are met")
 
 
 # End
