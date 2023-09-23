94ca23053263b22bee67b48f2b0ac0e9393d84ee
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-23 22:42:42 +0000

    Initial commit

--- directory_creation.py
+++ directory_creation.py
@@ -20,15 +20,15 @@ def list_files_and_directories(directory_name):
     
     # Script must use the os.walk() function from the os library
     for (root, dirs, files) in os.walk(directory_name):
-    ### Add a print command here to print ==root==
-    print("==root==") 
-    print(root)
-    ### Add a print command here to print ==dirs==
-    print("==dirs==")
-    print(dirs)
-    ### Add a print command here to print ==files==
-    print("==files==")
-    print(files)
+        ### Add a print command here to print ==root==
+        print("==root==") 
+        print(root)
+        ### Add a print command here to print ==dirs==
+        print("==dirs==")
+        print(dirs)
+        ### Add a print command here to print ==files==
+        print("==files==")
+        print(files)
 
 
 # Invoke the function
