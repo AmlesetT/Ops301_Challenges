a4c6470c88b1f98800e6fa9933763822e10bcce5
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-27 23:29:19 +0000

    Initial commit

--- python_collections.py
+++ python_collections.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Script Name:                  Ops 301 - Challenge 08
-# Author:                       Amleset Tesfamariam
+# Author:                       Amleset Tesfamariam, CF Ops lecture 301
 # Date of latest revision:      09/06/2023
 # Purpose:                      To create a script in Python that qualify certain conditions including: assigning a variable list to multiple string elements, printing elements from this list, and changing the values within this list.
 # Reason:                       It is important to know how to create, edit, find, and add to lists of different variables.
