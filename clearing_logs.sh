95d955febff233a496d354f7c700ed19363f1e25
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-27 23:27:26 +0000

    Initial commit

--- clearing_logs.sh
+++ clearing_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script Name:                  Ops 301 - Challenge 05
-# Author:                       Amleset Tesfamariam/Chat GPT
+# Author:                       Amleset Tesfamariam, CF Ops lecture 301, ChatGPT
 # Date of latest revision:      09/01/2023
 # Purpose:                      To clear a log using a bash script.
 # Reason:                       It is critical as an administrator to be able to maintain clear and organized logs.
