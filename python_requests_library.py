89ab623aa060cb6f81ca2ac9afad4dbcca4a631c
Amleset Tesfamariam <139098353+AmlesetT@users.noreply.github.com>
2023-09-27 23:32:28 +0000

    Initial commit

--- python_requests_library.py
+++ python_requests_library.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Script Name:                  Ops 301 - Challenge 12
-# Author:                       Amleset Tesfamariam, Google, ChatGPT
+# Author:                       Amleset Tesfamariam, CF Ops lecture 301, Google, ChatGPT
 # Date of latest revision:      09/12/2023
 # Purpose:                      To create a script using requests from the Python library.
 # Reason:                       It is crucial to understand the evaluation of web server requests and responses.
